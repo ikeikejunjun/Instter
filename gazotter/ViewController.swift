549dc5e32ed3094b52ff5c1a0cbd4269c0633d10
Junya Ikeda <junya.ikeda@frog-pod.com>
2021-04-03 09:26:42 +0900

    new xcode project

---
+++ gazotter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gazotter
+//
+//  Created by 池田 潤哉 on 2021/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
